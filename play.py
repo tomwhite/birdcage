f4b8c67f69231c9b23253a14c9e0e4b3a1e03b13
Tom White <tom.e.white@gmail.com>
2021-03-27 18:03:54 +0000

    Rewrite

---
+++ play.py
@@ -0,0 +1,35 @@
+from bridg_it import *
+
+from blessed import Terminal
+
+def play_interactive(board, player1, player2, M=3, term=None):
+    with term.fullscreen(), term.hidden_cursor():
+        print(term.move(0, 0) + "{} (W) - {} (B)".format(player1, player2))
+        print(term.move(1, 0) + str(board))
+        while True:
+            with term.cbreak(): # wait for key press
+                inp = term.inkey()
+            move = player1.play(board)
+            board = board.move(move)
+            print(term.move(1, 0) + str(board))
+            if board.white_has_won():
+                print(f"{player1} wins")
+                break
+
+            with term.cbreak(): # wait for key press
+                inp = term.inkey()
+            move = player2.play(board)
+            board = board.move(move)
+            print(term.move(1, 0) + str(board))
+            if board.black_has_won():
+                print(f"{player2} wins")
+                break
+        with term.cbreak(): # wait for key press
+            inp = term.inkey()
+
+if __name__ == '__main__':
+    term = Terminal()
+    board = BridgIt()
+    player1 = Shannon()
+    player2 = Random()
+    play_interactive(board, player1, player2, term=term)
---
+++ test_bridg_it.py
@@ -0,0 +1,105 @@
+from bridg_it import *
+from bridg_it import _to_alpha, _to_numeric
+import pytest
+from sympy import Rational
+
+"""                      
+     ● - ● - ●  
+5  ○   ○   ○   ○
+4  | ●   ●   ● |
+3  ○   ○   ○   ○
+2  | ●   ●   ● |
+1  ○   ○   ○   ○
+     ● - ● - ●  
+     A B C D E  
+"""
+
+def test_to_numeric():
+    assert _to_numeric("A1") == (1, 1)
+    assert _to_numeric("A2") == (1, 2)
+    assert _to_numeric("A3") == (1, 3)
+
+def test_to_alpha():
+    assert _to_alpha(1, 1) == ("A1")
+    assert _to_alpha(1, 2) == ("A2")
+    assert _to_alpha(1, 3) == ("A3")
+
+def test_is_valid_move():
+    assert not is_valid_move("A0")
+    assert is_valid_move("A1")
+    assert not is_valid_move("A2")
+    assert is_valid_move("A3")
+    assert not is_valid_move("A4")
+    assert is_valid_move("A5")
+    assert not is_valid_move("A6")
+
+    assert not is_valid_move("B0")
+    assert not is_valid_move("B1")
+    assert is_valid_move("B2")
+    assert not is_valid_move("B3")
+    assert is_valid_move("B4")
+    assert not is_valid_move("B5")
+    assert not is_valid_move("B6")
+
+    assert not is_valid_move("F2")
+
+def test_valid_moves():
+    VALID_MOVES = {'A1', 'A3', 'A5', 'B2', 'B4', 'C1', 'C3', 'C5', 'D2', 'D4', 'E1', 'E3', 'E5'}
+    assert set(valid_moves(M=3)) == VALID_MOVES
+
+def test_game():
+    b = BridgIt()
+    for move in ["A1", "C3", "C1", "C5", "E1"]:
+        assert not b.white_has_won()
+        assert not b.black_has_won()
+        b = b.move(move)
+    assert b.white_has_won()
+    assert not b.black_has_won()
+
+def test_bridg_it_long_game():
+    # from "Bridg-It – Beating Shannon’s Analog Heuristic" by Thomas Fisher
+    # https://www.minet.uni-jena.de//math-net/reports/sources/2009/09-07report.pdf
+    b = BridgIt()
+    for move in ["A5", "c5", "C3", "a1", "B4", "e3", "E1", "d2", "C1", "b2", "E5", "d4"]:
+        assert not b.white_has_won()
+        assert not b.black_has_won()
+        b = b.move(move.upper())
+        print()
+        print(b)
+    assert not b.white_has_won()
+    assert b.black_has_won()
+
+def test_birdcage_long_game():
+    bc = BirdCage()
+    for move in ["A5", "c5", "C3", "a1", "B4", "e3", "E1", "d2", "C1", "b2", "E5", "d4"]:
+        assert not bc.white_has_won()
+        assert not bc.black_has_won()
+        bc = bc.move(move.upper())
+        print()
+        print(bc)
+    assert not bc.white_has_won()
+    assert bc.black_has_won()
+
+def test_random():
+    bc = BirdCage()
+    r = Random()
+    move = r.play(bc)
+    bc.move(move)
+    print(bc)
+
+def test_shannon_voltage_diffs():
+    bc = BirdCage(moves=["A5", "C5"])
+    s = Shannon()
+    voltage_diffs = s._get_voltage_diffs(bc)
+    assert voltage_diffs["C3"] == Rational(129 - 58, 129)
+
+def test_shannon_game():
+    bc = BirdCage()
+    s = Shannon()
+    moves = ["A5", "c5", "C3", "a1", "B4", "e3", "E1", "d2"]
+    for m1, m2 in zip(*[iter(moves)] * 2):
+        print(m1, m2)
+        move = s.play(bc)
+        assert move == m1
+        bc.move(m1)
+        bc.move(m2.upper())
